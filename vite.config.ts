c2aa8b29f57647524c75d7729cd3505033aecbb2
daze <vogisa@mail.ru>
2023-03-04 11:05:11 +0300

    +sass & pug

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,25 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+// @ts-ignore
+import vuePugPlugin from 'vue-pug-plugin';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(
+			{
+				template: {
+					preprocessOptions: {
+						plugins: [
+							vuePugPlugin,
+						],
+					},
+				},
+			},
+		),
+	],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
