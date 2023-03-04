9bfb24e1d159bf05e6ff6fb173a2f6f740f4f05f
daze <vogisa@mail.ru>
2023-03-04 14:06:09 +0300

    +some initial styles and v-model to input

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,11 +1,15 @@
 <template lang="pug">
 label.no-input
-	span.no-input__label {{ label }}
-	input.no-input__input(:type="type")
+	input.no-input__input(:type="type" v-model="inputValue")
+	.no-input__label(:class="{active: inputValue}") {{ label }}
 </template>
 
 <script lang="ts">
-export default {
+import {defineComponent} from 'vue';
+
+import {ref} from 'vue';
+
+export default defineComponent({
 	name: 'NOInput',
 	props: {
 		label: String,
@@ -14,12 +18,66 @@ export default {
 			default: 'text',
 		},
 	},
-};
+	setup () {
+		const inputValue = ref<string>('');
+
+		return {inputValue};
+	},
+});
 </script>
 
 <style lang="scss">
+@mixin label-active {
+	top: -2.3rem;
+	font-size: 1rem;
+	width: fit-content;
+}
+
 .no-input {
 	display: flex;
 	flex-direction: column;
+	position: relative;
+	margin-top: 2rem;
+
+	* {
+		transition: var(--default-transition);
+	}
+
+	&__label {
+		font-size: 1.5rem;
+		position: absolute;
+		z-index: 2;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		padding-left: 0.75rem;
+		display: flex;
+		align-items: center;
+
+		&.active {
+			@include label-active;
+		}
+	}
+
+	&__input {
+		background-color: var(--color-background-mute);
+		border: 1px solid var(--color-border);
+		padding: 0.75rem;
+		outline: none;
+		color: var(--color-heading);
+		border-radius: 5px;
+		font-size: 1.5rem;
+
+		&:focus {
+			border-color: var(--color-border-hover);
+
+			+ .no-input {
+				&__label {
+					@include label-active;
+				}
+			}
+		}
+	}
 }
 </style>
