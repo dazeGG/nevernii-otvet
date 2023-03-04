43c444658cbbcb37595e6e863e0a794af7367fe6
daze <vogisa@mail.ru>
2023-03-05 02:33:47 +0300

    changed endpoint to create question

--- src/api/questions.ts
+++ src/api/questions.ts
@@ -2,6 +2,6 @@ import api from '@/api';
 
 const getQuestion = (id: number | string): Promise<Response> => api.get(`question/${id}/`);
 
-const createQuestion = (data: FormData): Promise<Response> => api.post('questionlist/', data);
+const createQuestion = (data: FormData): Promise<Response> => api.post('question/', data);
 
 export {getQuestion, createQuestion};
