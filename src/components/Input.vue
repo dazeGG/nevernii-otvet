d628faa8d5ff043da1dcdba1b74f1eb6094ba84d
daze <vogisa@mail.ru>
2023-03-04 13:03:58 +0300

    init input component

---
+++ src/components/Input.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+label.no-input
+	span.no-input__label {{ label }}
+	input.no-input__input(:type="type")
+</template>
+
+<script lang="ts">
+export default {
+	name: 'NOInput',
+	props: {
+		label: String,
+		type: {
+			type: String,
+			default: 'text',
+		},
+	},
+};
+</script>
+
+<style lang="scss">
+.no-input {
+	display: flex;
+	flex-direction: column;
+}
+</style>
