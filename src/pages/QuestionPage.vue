6de0f589471243cf2008dec72b1b2a1f4bb4b744
daze <vogisa@mail.ru>
2023-03-05 11:33:28 +0300

    Question -> QuestionPage[because of composition api]

---
+++ src/pages/QuestionPage.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+h1 Question Page
+</template>
+
+<script lang="ts" setup>
+import {onBeforeMount} from 'vue';
+import {useRoute} from 'vue-router';
+
+import type IQuestion from '@/interfaces/IQuestion';
+
+import {getQuestion} from '@/api/questions';
+
+
+const questionID: number = parseInt(String(useRoute().params.id), 10);
+
+const question: IQuestion = {
+	id: questionID,
+	question_text: 'ты пидор?',
+	answer_yes: 'да',
+	answer_no: 'нет',
+};
+
+onBeforeMount(() => {
+	// ! getQuestion from back
+	getQuestion(questionID).then(r => console.log(r)).catch(e => console.log(e));
+});
+</script>
+
+<style lang="scss">
+</style>
