4435c286263cd569b8716fc61199fd469da63890
daze <vogisa@mail.ru>
2023-03-05 23:55:56 +0300

    base url now without slash

--- src/api/questions.ts
+++ src/api/questions.ts
@@ -1,7 +1,7 @@
 import api from '@/api';
 
-const getQuestion = (id: number | string): Promise<Response> => api.get(`question/${id}/`);
+const getQuestion = (id: number | string): Promise<Response> => api.get(`/question/${id}/`);
 
-const createQuestion = (data: FormData): Promise<Response> => api.post('question/', data);
+const createQuestion = (data: FormData): Promise<Response> => api.post('/question/', data);
 
 export {getQuestion, createQuestion};
